78686809f3c339e3ed1c263deba3395c40c1ba3a
teleries <teleries@gmail.com>
2021-01-20 14:43:17 -0500

    Add files via upload

---
+++ challenge01.py
@@ -0,0 +1,20 @@
+# Cal Ries
+
+# challenge 01
+
+
+
+
+
+
+a = int(input("a? "))
+b = int(input("b? "))
+
+print(a+b)
+print(a-b)
+print(a*b)
+if (b==0):
+	print("quotient undefined")
+else:
+	print(a/b)
+print(a%b)
